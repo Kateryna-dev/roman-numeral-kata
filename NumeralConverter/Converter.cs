23a4e4f1e5a516c395c8fa409438ce58a734e607
Kateryna <dev.kateryna@gmail.com>
2022-09-12 09:44:08 +0100

    Convert from Arabic to Roman Numerals

--- NumeralConverter/Converter.cs
+++ NumeralConverter/Converter.cs
@@ -9,7 +9,40 @@ namespace RomanNumeralConverter
     public static class Converter
     {
         private static List<string> romanNumerals = new List<string> () {"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-        private static char[] romanSymbols = new char[] { 'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+        private static List<char> romanSymbols = new List<char>() { 'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+        static readonly Dictionary<int, string> numeralsDictionary = new ()
+        {
+            { 1, "I" },
+            { 2, "II" },
+            { 3, "III" },
+            { 4, "IV" },
+            { 5, "V" },
+            { 6, "VI" },
+            { 7, "VII" },
+            { 8, "VIII" },
+            { 9, "IX" },
+            { 10, "X" },
+            { 20, "XX" },
+            { 30, "XXX" },
+            { 40, "XL" },
+            { 50, "L" },
+            { 60, "LX" },
+            { 70, "LXX" },
+            { 80, "LXXX" },
+            { 90, "XC" },
+            { 100, "C" },
+            { 200, "CC" },
+            { 300, "CCC" },
+            { 400, "CD" },
+            { 500, "D" },
+            { 600, "DC" },
+            { 700, "DCC" },
+            { 800, "DCCC" },
+            { 900, "CM" },
+            { 1000, "M" },
+            { 2000, "MM" },
+            { 3000, "MMM" },
+        };
 
         public static int ConvertFromRoman(string romanNumber) 
         {
@@ -20,14 +53,21 @@ namespace RomanNumeralConverter
             return 0;
         }
 
-
-        public static string ConvertFromArabic(int arabicNumeral)
+        public static string ConvertFromArabic(int number)
         {
-            if (arabicNumeral < 11)
+            string result = string.Empty;
+            if (!ArabicInputIsCorrect(number)) return result;
+
+            for (int divisor = 1000; divisor >= 1; divisor /= 10)
             {
-                return romanNumerals[arabicNumeral];
+                var numberPart = number / divisor;
+                if (numberPart > 0)
+                {
+                    result += numeralsDictionary[numberPart * divisor];
+                    number -= numberPart * divisor;
+                }
             }
-            return "Not implemented yet"; ;
+            return result;
         }
 
         public static bool RomanNumeralInputIsCorrect(string inputString) 
@@ -48,5 +88,6 @@ namespace RomanNumeralConverter
             return true;
         }
 
+        public static bool ArabicInputIsCorrect(int number) => number < 4000 && number > 0;
     }
 }
--- RomanNumeralConverter.Tests/ConverterTests.cs
+++ RomanNumeralConverter.Tests/ConverterTests.cs
@@ -12,7 +12,13 @@ namespace RomanNumeralConverter.Tests
             Converter.ConvertFromArabic(1).Should().Be("I");
             Converter.ConvertFromArabic(7).Should().Be("VII");
             Converter.ConvertFromArabic(10).Should().Be("X");
+            Converter.ConvertFromArabic(93).Should().Be("XCIII");
+            Converter.ConvertFromArabic(71).Should().Be("LXXI");
+            Converter.ConvertFromArabic(500).Should().Be("D");
+            Converter.ConvertFromArabic(1000).Should().Be("M");
+            Converter.ConvertFromArabic(100).Should().Be("C");
         }
+
 
         [Test]
         public void ConvertFromRoman_Shoud_Return_Correct_Numeral_For_Different_Inputs()
@@ -34,7 +40,14 @@ namespace RomanNumeralConverter.Tests
             Converter.RomanNumeralInputIsCorrect("XIX").Should().Be(true);
             Converter.RomanNumeralInputIsCorrect("XLI").Should().Be(true);
             Converter.RomanNumeralInputIsCorrect("C").Should().Be(true);
+        }
 
+        [Test]
+        public void ArabicInputIsCorrect_Shoud_Return_True_For_Valid_Input()
+        {
+            Converter.ArabicInputIsCorrect(10).Should().Be(true);
+            Converter.ArabicInputIsCorrect(555).Should().Be(true);
+            Converter.ArabicInputIsCorrect(3999).Should().Be(true);
         }
 
         [Test]
